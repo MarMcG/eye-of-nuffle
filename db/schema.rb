e9850ba9e21e279b1dd7769fb8c5a74092cfaf1d
Martyn McGowan <martyn_mcgowan@yahoo.com>
2022-07-26 18:29:00 -0400

    creation of player model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_220731) do
+ActiveRecord::Schema.define(version: 2022_07_26_222717) do
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.integer "number"
+    t.integer "position_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["position_id"], name: "index_players_on_position_id"
+  end
 
   create_table "positions", force: :cascade do |t|
     t.string "name"
@@ -39,5 +48,6 @@ ActiveRecord::Schema.define(version: 2022_07_26_220731) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "players", "positions"
   add_foreign_key "positions", "races"
 end

0cb5b95a3b4c237591e417c3f8f9a7a82e9bdd05
Martyn McGowan <martyn_mcgowan@yahoo.com>
2022-07-26 18:45:03 -0400

    Team model creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_222717) do
+ActiveRecord::Schema.define(version: 2022_07_26_224156) do
 
   create_table "players", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2022_07_26_222717) do
     t.integer "position_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "team_id", null: false
     t.index ["position_id"], name: "index_players_on_position_id"
+    t.index ["team_id"], name: "index_players_on_team_id"
   end
 
   create_table "positions", force: :cascade do |t|
@@ -48,6 +50,16 @@ ActiveRecord::Schema.define(version: 2022_07_26_222717) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.integer "race_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["race_id"], name: "index_teams_on_race_id"
+  end
+
   add_foreign_key "players", "positions"
+  add_foreign_key "players", "teams"
   add_foreign_key "positions", "races"
+  add_foreign_key "teams", "races"
 end

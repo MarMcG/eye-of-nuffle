6f5497e040627a9a8039ca2b1395778cd03c662e
Martyn McGowan <martyn_mcgowan@yahoo.com>
2022-07-26 17:58:58 -0400

    rails new

---
+++ db/migrate/20220726214934_create_races.rb
@@ -0,0 +1,11 @@
+class CreateRaces < ActiveRecord::Migration[6.1]
+  def change
+    create_table :races do |t|
+      t.string :name
+      t.string :special
+      t.integer :tier
+
+      t.timestamps
+    end
+  end
+end
